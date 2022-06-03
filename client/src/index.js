f7e0c576281771145aa9863a5fed830b97905cca
tristan <mousy93@hotmail.com>
2022-06-03 17:33:38 +0800

    navigation renamed as header to match instructions | Nav still within header

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import { Navigation, Footer, Home, About, Contact } from "./components";
-import { default as Navigation } from "./components/Navigation";
+
+import { default as Header } from "./components/Header";
 import { default as Footer } from "./components/Footer";
 import { default as Home } from "./components/Home";
 import { default as About } from "./components/About";
@@ -12,7 +12,7 @@ import { default as Contact } from "./components/Contact";
 
 ReactDOM.render(
   <Router>
-    <Navigation />
+    <Header />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
