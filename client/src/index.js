99405bb934ed5d3097536fc20a1cddb502d27c3d
tristan <mousy93@hotmail.com>
2022-06-03 17:22:35 +0800

    export error fixed | index removed from components not needed

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Navigation, Footer, Home, About, Contact } from "./components";
+// import { Navigation, Footer, Home, About, Contact } from "./components";
+import { default as Navigation } from "./components/Navigation";
+import { default as Footer } from "./components/Footer";
+import { default as Home } from "./components/Home";
+import { default as About } from "./components/About";
+import { default as Contact } from "./components/Contact";
 
 ReactDOM.render(
   <Router>
