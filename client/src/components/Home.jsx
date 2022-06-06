2c970c6ec1883a6250a18e04f78e170ad7c558b4
tristan <mousy93@hotmail.com>
2022-06-06 12:52:59 +0800

    playing with letter styling | refactored Homejsx

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,36 +2,49 @@ import React from "react";
 
 function Home() {
   return (
-    <div className="home">
-      <div class="Container">
-        <div className="grid-container">
-          <div class="row align-items-center ">
-            <div class="col-lg-5">
-              <div className="pTagColorDiv">
-                <p className="pTagColor">Hey there, my name is</p>
-              </div>
-              <div className="h1ColorChangeDiv">
-                <h1 className="h1ColorChange" class="font-weight-light">
-                  Tristan Fontanini,
-                </h1>
-              </div>
-              <div className="pTagDiv">
-                <p>
-                  I am a forever learning full stack developer who focuses more
-                  on the front end side. I really enjoy design and creating
-                  interesting UI/UX design where it fits. I have mostly worked
-                  with Html, CSS, Javascript, ReactJS and C#. Currently I am
-                  learning VR development, game dev and learning about machine
-                  learning and how I can make it work with gaming. Most of my
-                  experience is around web development and finding automated
-                  solutions. I get excited learning new things and plan to
-                  continue growing on my coding journey.
-                </p>
-              </div>
-              <div className="imgTagDiv">
-                <p className="imgTag"> image here replace with img tag</p>
-              </div>
-            </div>
+    <div className="homeContainer">
+      <div className="grid-container">
+        <div>
+          <div className="helloDiv">
+            <p className="helloMessage">Hey there, my name is</p>
+          </div>
+          <div className="h1Div">
+            <h1 class="font-weight-light">
+              <span id="nameT1">T</span>
+              <span id="nameR">r</span>
+              <span id="nameI1">i</span>
+              <span id="nameS">s</span>
+              <span id="nameT2">t</span>
+              <span id="nameA1">a</span>
+              <span id="nameN1">n</span>
+              <span> </span>
+              <span id="nameF">F</span>
+              <span id="nameO">o</span>
+              <span id="nameN1">n</span>
+              <span id="nameT3">t</span>
+              <span id="nameA2">a</span>
+              <span id="nameN2">n</span>
+              <span id="nameI2">i</span>
+              <span id="nameN3">n</span>
+              <span id="nameI3">i</span>
+              <span id="nameCom">,</span>
+            </h1>
+          </div>
+          <div className="pTagDiv">
+            <p>
+              I am a forever learning full stack developer who focuses more on
+              the front end side. I really enjoy design and creating interesting
+              UI/UX design where it fits. I have mostly worked with Html, CSS,
+              Javascript, ReactJS and C#. Currently I am learning VR
+              development, game dev and learning about machine learning and how
+              I can make it work with gaming. Most of my experience is around
+              web development and finding automated solutions. I get excited
+              learning new things and plan to continue growing on my coding
+              journey.
+            </p>
+          </div>
+          <div className="imgTagDiv">
+            <p className="imgTag"> image here replace with img tag</p>
           </div>
         </div>
       </div>
