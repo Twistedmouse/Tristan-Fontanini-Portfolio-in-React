911336dd882e21ed2bc303b640966d282a4aad57
tristan <mousy93@hotmail.com>
2022-06-07 15:24:50 +0800

    grid fixed remove stay div from home

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,51 +4,49 @@ function Home() {
   return (
     <div className="homeContainer">
       <div className="grid-container">
-        <div>
-          <div className="helloDiv">
-            <p className="helloMessage">Hey there, my name is</p>
-          </div>
-          <div className="h1Div">
-            <h1 class="font-weight-light">
-              <span id="nameT1">T</span>
-              <span id="nameR">r</span>
-              <span id="nameI1">i</span>
-              <span id="nameS">s</span>
-              <span id="nameT2">t</span>
-              <span id="nameA1">a</span>
-              <span id="nameN1">n</span>
-              <span> </span>
-              <span id="nameF">F</span>
-              <span id="nameO">o</span>
-              <span id="nameN1">n</span>
-              <span id="nameT3">t</span>
-              <span id="nameA2">a</span>
-              <span id="nameN2">n</span>
-              <span id="nameI2">i</span>
-              <span id="nameN3">n</span>
-              <span id="nameI3">i</span>
-              <span id="nameCom">,</span>
-            </h1>
-          </div>
-          <div className="pTagDiv">
-            <p>
-              I am a forever learning full stack developer who focuses more on
-              the front end side. I really enjoy design and creating interesting
-              UI/UX design where it fits. I have mostly worked with Html, CSS,
-              Javascript, ReactJS and C#.
-            </p>
+        <div className="helloDiv">
+          <p className="helloMessage">Hey there, my name is</p>
+        </div>
+        <div className="h1Div">
+          <h1 class="font-weight-light">
+            <span id="nameT1">T</span>
+            <span id="nameR">r</span>
+            <span id="nameI1">i</span>
+            <span id="nameS">s</span>
+            <span id="nameT2">t</span>
+            <span id="nameA1">a</span>
+            <span id="nameN1">n</span>
+            <span> </span>
+            <span id="nameF">F</span>
+            <span id="nameO">o</span>
+            <span id="nameN1">n</span>
+            <span id="nameT3">t</span>
+            <span id="nameA2">a</span>
+            <span id="nameN2">n</span>
+            <span id="nameI2">i</span>
+            <span id="nameN3">n</span>
+            <span id="nameI3">i</span>
+            <span id="nameCom">,</span>
+          </h1>
+        </div>
+        <div className="pTagDiv">
+          <p>
+            I am a forever learning full stack developer who focuses more on the
+            front end side. I really enjoy design and creating interesting UI/UX
+            design where it fits. I have mostly worked with Html, CSS,
+            Javascript, ReactJS and C#.
+          </p>
 
-            <p>
-              Currently I am learning VR development, game dev and learning
-              about machine learning and how I can make it work with gaming.
-              Most of my experience is around web development and finding
-              automated solutions. I get excited learning new things and plan to
-              continue growing on my coding journey.
-            </p>
-          </div>
-          <div className="imgTagDiv">
-            <p className="imgTag"> image here replace with img tag</p>
-          </div>
+          <p>
+            Currently I am learning VR development, game dev and learning about
+            machine learning and how I can make it work with gaming. Most of my
+            experience is around web development and finding automated
+            solutions. I get excited learning new things and plan to continue
+            growing on my coding journey.
+          </p>
+        </div>
+        <div className="imgTagDiv">
+          <p className="imgTag"> image here replace with img tag</p>
         </div>
       </div>
     </div>
