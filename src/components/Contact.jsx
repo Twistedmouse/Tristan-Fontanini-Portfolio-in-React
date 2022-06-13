58612d2f7780e597a9477fb7e860e3aac7a523f4
tristan <mousy93@hotmail.com>
2022-06-13 16:18:06 +0800

    contact form css updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,63 +1,58 @@
 import React from "react";
-import { useRef } from "react";
+import emailjs from "emailjs-com";
 
 function Contact() {
-  const firstNameRef = useRef(null);
-  const lastNameRef = useRef(null);
-  const emailRef = useRef(null);
-  const messageRef = useRef(null);
+  function handleSubmit(e) {
+    e.preventDefault();
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+    emailjs.sendForm();
+  }
 
-    const data = {
-      firstName: firstNameRef.current.value,
-      lastName: lastNameRef.current.value,
-      email: emailRef.current.value,
-      message: messageRef.current.value,
-    };
-  };
   return (
-    <div className="contactContainer">
-      <div class="col-lg-5">
-        <h1 class="font-weight-light">Contact</h1>
-        <form onSubmit={handleSubmit} className="form">
-          <div className="name">
-            <label for="firstName">First name</label>
-            <input
-              type="text"
-              name="firstName"
-              className="firstName"
-              tabIndex="1"
-            />
-            <label for="lastName">Last name</label>
-            <input
-              type="text"
-              name="lastName"
-              className="lastName"
-              tabIndex="2"
-            />
-            <label for="email">Email</label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              className="email"
-              placeholder="example@corp.com"
-              tabIndex="3"
-            />
-            <label for="message">Message</label>
+    <div className="contact-grid-container">
+      <div className="contactContainer">
+        <div class="col-lg-5">
+          <h1 class="contactH1">Let's get in touch:</h1>
+          <form onSubmit={handleSubmit} className="form">
+            <div className="name">
+              <label for="firstName">First name:</label>
+              <input
+                type="text"
+                name="firstName"
+                className="firstName"
+                tabIndex="1"
+              />
+              <label for="lastName">Last name:</label>
+              <input
+                type="text"
+                name="lastName"
+                className="lastName"
+                tabIndex="2"
+              />
+              <div className="email">
+                <label for="email">Email:</label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="email"
+                  placeholder="example@email.com"
+                  tabIndex="3"
+                />
+              </div>
+              <label for="message">Message:</label>
 
-            <textarea
-              placeholder="Start typing..."
-              className="message"
-              name="message"
-            />
-          </div>
-          <button type="submit" className="send">
-            Send
-          </button>
-        </form>
+              <textarea
+                placeholder="Start typing..."
+                className="message"
+                name="message"
+              />
+            </div>
+            <button type="submit" className="send">
+              Send
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
