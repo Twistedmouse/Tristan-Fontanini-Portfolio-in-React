9417cf210e6384b2ec2ee98a82c76ef3fc4670d7
tristan <mousy93@hotmail.com>
2022-06-17 15:35:41 +0800

    a tags added to prjoect work to link to repo or live page | started media queires

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -14,14 +14,64 @@ function Project() {
       <h1 className="ProjectH1">Project Work</h1>
       <ul className="workContainer">
         <p class="hiddenText">Click To View Repo.</p>
-        <img class="work" src={jsQuiz} alt="js-quiz-img" />
-        <img class="work" src={scratchGame} alt="scratch-game-img" />
-        <img class="work2" src={expressYourself} alt="express-yourself-img" />
-        <img class="work" src={iCanHelp} alt="i-can-help-img" />
-        <img class="work" src={dayPlan} alt="daily-planner-img" />
-        <img class="work2" src={weather} alt="weather-tracker-img" />
-        <img class="work" src={passwordGen} alt="password-generator-img" />
-        <img class="work" src={myMind} alt="my-mind-matters-img" />
+        <a
+          href="https://twistedmouse.github.io/Javascript-Interactive-Quiz/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img class="work" src={jsQuiz} alt="js-quiz-img" />
+        </a>
+        <a
+          href="https://scratch.mit.edu/projects/483253369"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img class="work" src={scratchGame} alt="scratch-game-img" />
+        </a>
+
+        <a
+          href="https://github.com/Twistedmouse/EXPRESS-yoURSELF"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img class="work2" src={expressYourself} alt="express-yourself-img" />
+        </a>
+
+        <a
+          href="https://github.com/Twistedmouse/I-CAN-HELP-WorkMarketPlace"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img class="work" src={iCanHelp} alt="i-can-help-img" />
+        </a>
+        <a
+          href="https://github.com/Twistedmouse/Daily-Scheduler"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img class="work" src={dayPlan} alt="daily-planner-img" />
+        </a>
+        <a
+          href="https://github.com/Twistedmouse/WEATHER-DASHBOARD"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img class="work2" src={weather} alt="weather-tracker-img" />
+        </a>
+        <a
+          href="https://github.com/Twistedmouse/Random-Password-Generator"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img class="work" src={passwordGen} alt="password-generator-img" />
+        </a>
+        <a
+          href="https://github.com/Twistedmouse/My-Mind-Matters"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img class="work" src={myMind} alt="my-mind-matters-img" />
+        </a>
       </ul>
     </div>
   );
