f71017825e0acb14d2223390b4d62b8ed29bea52
tristan <mousy93@hotmail.com>
2022-06-18 15:45:49 +0800

    tooltips added to project work img a tags

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -13,9 +13,10 @@ function Project() {
     <div className="projectContainer">
       <h1 className="ProjectH1">Project Work</h1>
       <ul className="workContainer">
-        <p class="hiddenText">Click To View Repo.</p>
+        <p class="hiddenText">Click Image To View Repo.</p>
         <a
           href="https://twistedmouse.github.io/Javascript-Interactive-Quiz/"
+          data-tool-tip="Click to view"
           target="_blank"
           rel="noreferrer"
         >
@@ -23,6 +24,7 @@ function Project() {
         </a>
         <a
           href="https://scratch.mit.edu/projects/483253369"
+          data-tool-tip="Click to view"
           target="_blank"
           rel="noreferrer"
         >
@@ -30,7 +32,9 @@ function Project() {
         </a>
 
         <a
+          className="longTooltip"
           href="https://github.com/Twistedmouse/EXPRESS-yoURSELF"
+          data-tool-tip="Click to view"
           target="_blank"
           rel="noreferrer"
         >
@@ -39,34 +43,43 @@ function Project() {
 
         <a
           href="https://github.com/Twistedmouse/I-CAN-HELP-WorkMarketPlace"
+          data-tool-tip="Click to view"
           target="_blank"
           rel="noreferrer"
         >
           <img class="work" src={iCanHelp} alt="i-can-help-img" />
         </a>
         <a
+          class="dailySchedToolTip"
           href="https://github.com/Twistedmouse/Daily-Scheduler"
           target="_blank"
+          data-tool-tip="Click to view"
           rel="noreferrer"
         >
           <img class="work" src={dayPlan} alt="daily-planner-img" />
         </a>
         <a
+          className="longTooltip2"
           href="https://github.com/Twistedmouse/WEATHER-DASHBOARD"
+          data-tool-tip="Click to view"
           target="_blank"
           rel="noreferrer"
         >
           <img class="work2" src={weather} alt="weather-tracker-img" />
         </a>
         <a
+          class="bottomRowWork"
           href="https://github.com/Twistedmouse/Random-Password-Generator"
+          data-tool-tip="Click to view"
           target="_blank"
           rel="noreferrer"
         >
           <img class="work" src={passwordGen} alt="password-generator-img" />
         </a>
         <a
+          class="bottomRowWork"
           href="https://github.com/Twistedmouse/My-Mind-Matters"
+          data-tool-tip="Click to view"
           target="_blank"
           rel="noreferrer"
         >
