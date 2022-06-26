defc69414f5db01ef6764fbeffd5a23a1ad1de81
tristan <mousy93@hotmail.com>
2022-06-26 12:47:03 +0800

    changed BrowserRouter to HashRouter index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./style.css";
 import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 import { default as Header } from "./components/Header";
 import { default as Footer } from "./components/Footer";
@@ -10,7 +11,6 @@ import { default as Home } from "./components/Home";
 import { default as About } from "./components/About";
 import { default as Project } from "./components/Project";
 import { default as Contact } from "./components/Contact";
-
 ReactDOM.render(
   <Router>
     <Header />
